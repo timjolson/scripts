c9755d2455b75ed838f7c5e17f26a6001cf53bcd
timjolson <timjolson@users.noreply.github.com>
2026-02-22 01:51:26 -0600

    cleaned up script, improved cleanup, removed debugging logs, ready for usage.

--- overlay-in-place.sh
+++ overlay-in-place.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Script to combine, via mergerfs overlay, branches into an existing destination directory.
-# Creates a temporary bind mount of the original destination, then mounts the merged directory on top of the original destination.
+# Script to combine branches into an in-place overlay, via mergerfs. Supports non-in-place overlays as well.
+# For in-place overlays, creates temporary mounts of the destination, then mounts the merged directory on top of the destination.
 #
+# Note: this script forces "-f" so that it continues running for shutdown cleanup, and "flush-on-close=always" to help prevent data loss.
 # Note: this is UNRELIABLE if "=" is in the path of any branch, as the script uses "=" to identify a specified write-mode
-# assignment for each branch.
+# assignment for each branch. TODO: improve the parsing logic to handle "=" in paths and recognize write-mode assignments more robustly.
 # 
-# Usage: overlay-in-place.sh <destination dir> <branches str> <options formatted for mergerfs>
+# Usage: overlay-in-place.sh <branches> <destinatino> <options formatted for mergerfs>
 # Examples: 
-#    overlay-in-place.sh Documents "Video:Music"
-#    overlay-in-place.sh "Pictures" Pictures=NC:Video:Music -o fsname=in-place-overlay
-#    overlay-in-place.sh Pictures Pictures=NC:"with space":Music
+#    overlay-in-place.sh "Video:Music" Documents
+#    overlay-in-place.sh Pictures=NC:Video:Music "Pictures" -o fsname=in-place-overlay
+#    overlay-in-place.sh Pictures=NC:"with space":Music Pictures
 # 
 # 
 # https://github.com/trapexit/mergerfs/releases/download/2.41.1/mergerfs_2.41.1.debian-bookworm_amd64.deb
@@ -20,96 +21,135 @@ log() {
         local message=""
         # Check if there is piped input
         if [ -p /dev/stdin ]; then
-                # Read from stdin if there is any input
-                read -r message || message=""  # Read from stdin, default to empty if read fails
+            # Read from stdin if there is any input
+            read -r message || message=""  # Read from stdin, default to empty if read fails
         fi
         # If no piped input, check for the first argument
         if [[ -z "$message" && $# -gt 0 ]]; then
-                message="$@"
+            message="$@"
         fi
         # If still no message
-        if [[ -z "$message" ]]; then
-                return
+        if [[ ! -z "$message" ]]; then
+            echo "$message"
         fi
-        # log to journal
-        echo "$message"
 }
 
-[ $# -ge 2 ] || { log "Usage $0 <destination dir> <branches str> <options for mergerfs>"; exit 2; }
+[ $# -ge 2 ] || { log "Usage $0 <destination> <branches> <options for mergerfs>"; exit 2; }
 
+## Get parameters
+# Incoming branches string, e.g. "Video:Music" or "Pictures=NC:Video:Music"
+branches_in="$1"
 
-dest="$1"
-dest="${dest%%/}" # remove trailing slash if present
+# Resolve the real path of the destination and check if it is a directory.
+dest="$2"
+dest="${dest%%/}" # Remove trailing slash if any
 real_dest=$(realpath "$dest") || { log "Failed to resolve real path of \"$dest\"."; exit 1; }
 if [[ ! -d "$real_dest" ]]; then
     log "Destination \"$dest\" does not exist or is not a directory."
     exit 1
 fi
-branches_str="$2"
 shift 2
 
+# Get the remaining arguments for mergerfs, if any.
 remaining_args=("$@")
 
-# output a summary of the configuration
-log "Overlaying \"$dest\" with \"$branches_str\"."
-log "Remaining mergerfs args: ${remaining_args[*]}"
+## Prepare for in-place overlay handling.
+# Initialize variables for in-place overlay handling
+inplace=false
+temp_dir=""
+bind=""
+merged=""
 
-# create temporary directories for bind mount and merged mount
-temp_dir=$(mktemp -d .XXXXXXXXXXXX)
-bind="$temp_dir/bind-$dest"
-merged="$temp_dir/merged-$dest"
+make_temps() {
+    # Create temporary directories for bind mount and merged mount
+    temp_dir=$(mktemp -d $dest.XXXXXXXXXXXX)
+    bind="$temp_dir/bind-$dest"
+    merged="$temp_dir/merged-$dest"
+}
 
-## Handle the destination being a branch in the branches string.
+## Handle the target being a branch in the branches string.
 # Split branches into array by ':'.
-IFS=':' read -r -a branches_array <<< "$branches_str"
+IFS=':' read -r -a branches_array <<< "$branches_in"
 for i in "${!branches_array[@]}"; do
-    # Remove trailing slash if present, then split into branch_base and suffix by the first '=' character.
+    # Remove trailing slash
     branches_array[$i]="${branches_array[$i]%%/}"
+    # Split from the first '=' (write-mode)
     branch_base="${branches_array[$i]%%=*}"
+    # Get the real path of the branch
     real_branch_base=$(realpath "$branch_base") || { log "Failed to resolve real path of branch \"$branch_base\"."; exit 1; }
+    # Get the '=' (write-mode)
     suffix="${branches_array[$i]#${branch_base}}"
+
+    # If the real path of the branch matches the real path of the destination, replace the branch with the bind mount 
+    # and flag as an in-place overlay.
     if [[ "$real_branch_base" == "$real_dest" ]]; then
+        [[ -z "$temp_dir" ]] && make_temps
         branches_array[$i]="$bind$suffix"
+        inplace=true
     fi
 done
-# Combine branches_array into a string delimited by ':'
-branches="$(IFS=:; echo "${branches_array[*]}")"
-log "branches = \"$branches\""
 
-# cleanup temporary directories and mounts on exit
+## Create branches string for mergerfs call
+if [[ "$inplace" = true ]]; then
+    # concatenate branches back into a string for mergerfs
+    branches="$(IFS=:; echo "${branches_array[*]}")"
+    branch_msg=" Branches resolved to \"$branches\"."
+    mergerfs_path="$merged"
+else
+    # standard mergerfs mount
+    branches="$branches_in"
+    branch_msg=""
+    mergerfs_path="$dest"
+fi
+
+# Log the configuration
+log "Overlaying \"$dest\" with \"$branches_in\".$branch_msg"
+[[ -z "$remaining_args" ]] || log "Options for mergerfs: ${remaining_args[*]}"
+
+## Set up traps to ensure cleanup on exit or interruption, applies to the remainder of the script.
 cleanup() {
-    fusermount -u "$dest" | log
-    fusermount -u "$merged" | log
-    fusermount -u "$bind" | log
-    umount "$dest" | log
-    umount "$merged" | log
-    umount "$bind" | log
-    rm -rdf "$temp_dir" | log
+    # Cleanup temporary directories and mounts on exit
+    [[ -d "$mergerfs_path" ]] && $( umount "$mergerfs_path" || fusermount -u "$mergerfs_path" ) | log
+
+    if [[ "$inplace" = true ]]; then
+        # Unmount the bind mount and merged mount, and remove the temporary directory.
+        [[ -d "$dest" ]] && $( umount "$dest" || fusermount -u "$dest") | log
+        [[ ( ! -z "$bind" ) && -d "$bind" ]] && $( umount "$bind" || fusermount -u "$bind" ) | log
+        [[ ( ! -z "$temp_dir" ) && -d "$temp_dir" ]] && rm -rdf "$temp_dir" | log
+    fi
     exit $1
 }
-trap 'cleanup' SIGINT SIGTERM EXIT
+trap 'cleanup' SIGINT SIGTERM
 
-read -p "Make bind and merged directories?..."
-[ -d "$bind" ] || { log "Bind directory \"$bind\" does not exist. Creating it."; mkdir -p "$bind"; } || { log "Failed to create bind directory \"$bind\"."; cleanup 1; }
-[ -d "$merged" ] || { log "Merged directory \"$merged\" does not exist. Creating it."; mkdir -p "$merged"; } || { log "Failed to create merged directory \"$merged\"."; cleanup 1; }
+## Prepare for in-place overlay handling.
+if [[ "$inplace" = true ]]; then
+    # Create the bind directory (as a layer of obfuscation to prevent mergerfs from treating the original destination as a branch).
+    [ -d "$bind" ] || { mkdir -p "$bind"; } || { log "Failed to create bind directory \"$bind\"."; cleanup 1; }
+    mount --bind "${dest}" "${bind}" || { log "Failed to bind mount \"$dest\" to \"$bind\"."; cleanup 1; }
 
-# bind mount the original destination directory to the temprary bind directory
-read -p "Bind mount \"$dest\" to \"$bind\"?..."
-mount --bind "${dest}" "${bind}" || { log "Failed to bind mount \"$dest\" to \"$bind\"."; cleanup 1; }
-log "Make \"$bind\" private?..."
-mount --make-private "${bind}" || { log "Failed to make \"$bind\" private."; cleanup 1; }
+    # Make the bind mount private to prevent propagation of mounts/unmounts to the original destination. This prevents
+    # mergerfs from failing due to a branch being the destination.
+    mount --make-private "${bind}" || { log "Failed to make \"$bind\" private."; cleanup 1; }
 
-# use mergerfs to overlay the source and bind directories on top of the original destination directory
-read -p "Mount the merge to \"$merged\"?..."
+    # Create the merged directory for the mergerfs mount.
+    [ -d "$merged" ] || { mkdir -p "$merged"; } || { log "Failed to create merged directory \"$merged\"."; cleanup 1; }
+fi
+
+# Do the mergerfs mount. mergerfs_path depends on whether this is an in-place overlay or not.
 /usr/bin/mergerfs \
+    -f \
     "${branches}" \
-    "${merged}" \
+    "${mergerfs_path}" \
+    -o flush-on-close=always \
     "${remaining_args[@]}" 2>&1 | log &
 mergerfs_pid=$!
 
-log "Mount overlay of \"$merged\" back to \"$dest\"?..."
-mount --bind "${merged}" "${dest}" || { log "Failed to bind mount back to \"$dest\"."; cleanup 1; }
-log "Mounted merge at \"$dest\"."
+# If the overlay is in-place, mount the merged directory on top of the original destination.
+if [[ "$inplace" = true ]]; then
+    mount --bind "${merged}" "${dest}" || { log "Failed to bind mount back to \"$dest\"."; cleanup 1; }
+fi
 
-read -p "Press Enter to unmount and exit..."
+log "Mounted overlay at \"$dest\"."
+wait $mergerfs_pid
+
 cleanup 0
