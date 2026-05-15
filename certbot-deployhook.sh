bf03002df8a027bace164ca470d27d2a3f32bb77
timjolson <timjolson@users.noreply.github.com>
2026-05-15 17:45:10 -0500

    added dietpi-dashboard cert handling

--- certbot-deployhook.sh
+++ certbot-deployhook.sh
@@ -5,6 +5,7 @@ echo "$(date) renewing certs DEPLOY-HOOK" >> $logpath
 le_dir="/etc/letsencrypt/live"
 dns_name="awyw.crabdance.com"
 syncthing_dir="/mnt/dietpi_userdata/syncthing"
+dietpi_dashboard_dir="/opt/dietpi-dashboard"
 whitelistpath="/var/log/whitelist.log"
 
 ### stop running services
@@ -12,6 +13,8 @@ whitelistpath="/var/log/whitelist.log"
 # sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --on >> $logpath
 echo "$(date) stopping syncthing" >> $logpath
 systemctl stop syncthing >> $logpath
+systemctl stop dietpi-dashboard-frontend >> $logpath
+systemctl stop dietpi-dashboard-backend >> $logpath
 
 
 ### handle syncthing certs
@@ -35,16 +38,38 @@ chown syncthing:dietpi -R $syncthing_dir
 chmod u=rwX,g=rwX,o=r $syncthing_dir
 
 
+### handle dietpi-dashboard certs
+# backup existing
+echo "$(date) mkdir -p $dietpi_dashboard_dir/bak" >> $logpath
+mkdir -p $dietpi_dashboard_dir/bak
+echo "$(date) mv dietpi-dashboard/*.pem $dietpi_dashboard_dir/bak/" >> $logpath
+mv $dietpi_dashboard_dir/privkey.pem $dietpi_dashboard_dir/bak/privkey.pem
+mv $dietpi_dashboard_dir/cert.pem $dietpi_dashboard_dir/bak/cert.pem
+
+# copy new cert
+echo "$(date) cp fullchain.pem -> cert.pem" >> $logpath
+cp -f $le_dir/$dns_name/fullchain.pem $dietpi_dashboard_dir/cert.pem
+chown dietpi-dashboard-frontend:root $dietpi_dashboard_dir/cert.pem
+chmod u=rwX,g=rwX,o=r $dietpi_dashboard_dir/cert.pem
+echo "$(date) cp privkey.pem -> privkey.pem" >> $logpath
+cp -f $le_dir/$dns_name/privkey.pem $dietpi_dashboard_dir/privkey.pem
+chown dietpi-dashboard-frontend:root $dietpi_dashboard_dir/privkey.pem
+chmod u=rwX,g=rwX,o=r $dietpi_dashboard_dir/privkey.pem
+
+
 ### put new WAN IP address into whitelist
 MYIP=$(curl -s ipinfo.io/ip | grep -Eo '^([0-9]{1,3}\.){3}[0-9]{1,3}$')
 echo "$(date) Logging WAN IP ($MYIP) to '$whitelistpath'" >> $logpath
 echo "### WAN IP address. Updated from letsencrypt deployhook." > $whitelistpath
 echo "$(date) $MYIP" >> $whitelistpath
 
+
 ### restart services
 echo "$(date) restarting syncthing" >> $logpath
-systemctl restart syncthing >> $logpath
-
+/usr/local/bin/scripts/start-if-enabled.sh syncthing >> $logpath
+echo "$(date) restarting dietpi-dashboard" >> $logpath
+/usr/local/bin/scripts/start-if-enabled.sh dietpi-dashboard-frontend >> $logpath
+/usr/local/bin/scripts/start-if-enabled.sh dietpi-dashboard-backend >> $logpath
 # echo "$(date) nextcloud restarting" >> $logpath
 # sudo -u www-data php /var/www/nextcloud/occ maintenance:mode --off >> $logpath
 
